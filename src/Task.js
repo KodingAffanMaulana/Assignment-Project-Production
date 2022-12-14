02c0f22d88ecdbf31734558853baf390a2959383
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2022-12-14 22:14:32 +0700

    Commit all file

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Affan Maulana" // TODO: replace this
+const studentId = "FE2787726" // TODO: replace this
+
+export { url, studentName, studentId }
